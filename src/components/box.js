590fbe0666785419acb437f665b1e8def73cbffe
SIDERODROMOPHOBE <salibaa.gabriel@gmail.com>
2026-02-18 10:27:31 +0100

    Adding Message displayer

--- src/components/box.js
+++ src/components/box.js
@@ -1,9 +1,34 @@
+import React,{ useState } from "react";
+
+import Message from "./message";
+
+
 function Box() {
+  
+  const [messageCount, setMessageCount] = useState(0);
+  const [messageList, setMessageList] = useState([]);
+  
+  function incrementMessageCount() {
+    setMessageCount(messageCount + 1);
+  }
+  function addMessage() {;
+    setMessageList([...messageList, `Message ${messageCount}`]);
+  }
+  
   return (
     <div className="box">
         <h1>Box Component</h1>
-        <h1>Box Component</h1>
+        {messageList.map((message, index) => (
+          <Message key={index} text={message} />
+        ))}
+        <a href="#" onClick={() => { incrementMessageCount(); addMessage(); }}>
+          <h2>
+            Send Message {messageCount}
+          </h2>
+          
+        </a>
     </div>
+    
   );
 }
 
---
+++ src/components/message.js
@@ -0,0 +1,10 @@
+function Message({ text }) {
+  
+  return (
+    <div className="Message">
+        <h1>{text}</h1>
+    </div>
+  );
+}
+
+export default Message;
